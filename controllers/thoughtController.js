b16844a01c38690431e367517746ea7745cb1033
jake rudlong <jrudlong@gmail.com>
2023-04-02 15:07:59 -0500

    comments out controller files

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+// import the router
+
+// GET all thoughts
+
+// GET single thought by id
+
+// POST a new thought --> push thought-IDs to associated user's thoughts array
+
+// PUT -- update thought by id
+
+// DELETE thought by id
+
+// /:thoughtId/reactions
+
+// POST --> create a reaction, store in a thought's reaction array
+
+// DELETE --> pull/remove a reaction by reactionID
---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+// GET all users
+
+// GET a single user with thought & friend data
+
+// POST new user
+
+// PUT to update user by id
+
+// DELETE user by id
+
+// POST to add friend to user's friend list
+
+// DELETE - remove frind from user's friend list
+
+// **BONUS** DELETE user --> delete thought's associated with that user
